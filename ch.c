1d7d5311139d355bec87903c5c7f0ae729c8d564
nivi1997 <nivedhinisutha@gmail.com>
2017-09-24 11:53:03 +0530

    Update ch.c

--- ch.c
+++ ch.c
@@ -4,16 +4,16 @@ main()
 {
 char ch;
 scanf("%c",&ch);
-if(isUpper(ch))
+if(isupper(ch))
 {
-if(ch=='A'||ch=='E'||ch=='I'||ch='O'||ch='U')
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 printf("Vowel");
 else
 printf("Consonant");
 }
 else
 {
-if(ch=='a'||ch=='e'||ch=='i'||ch='o'||ch='u')
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 printf("Vowel");
 else
 printf("Consonant");
